ae731904d304f71ef355f1f7eec7383cf587cd33
Jared <redlaward@gmail.com>
2022-02-24 16:40:03 -0500

    commit tree 7195b2ef66c41f86ef10763a8a72edf1cd278cba

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record){
+    const result = record.find(elm => elm.result === "W" )
+    if (result) {
+        return result.year 
+    }
+    return undefined
+}
